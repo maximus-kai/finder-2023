8d4fc29814df83095352c163333b0b883359dd41
Dairo Tomiwa <38611128+maximus-kai@users.noreply.github.com>
2023-02-28 19:46:15 +0100

    another test of the supdates branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          a react fragment
+          a react fragmentgit
         </p>
         <a
           className="App-link"

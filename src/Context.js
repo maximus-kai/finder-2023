07b44df214a61a24c72d5c19e92e18cef4416989
Dairo Tomiwa <38611128+maximus-kai@users.noreply.github.com>
2023-03-05 17:21:48 +0100

    march 5 reactivated heroku cors temporaritly

--- src/Context.js
+++ src/Context.js
@@ -9,18 +9,18 @@ export class Provider extends Component {
         heading: 'Top 10 Tracks'
     }
     componentDidMount(){
-        axios.get()
+        axios.get(`https://cors-anywhere.herokuapp.com/api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=US&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
         .then(res => console.log(res.data))
         .catch(err => console.log(err));
     }
     render() {
         return (
             <Context.Provider value = {this.state}>
-        {this.props.children}
-      </Context.Provider>
-    )
-}
-}
+                 {this.props.children}
+              </Context.Provider>
+    );
+};
+};
 
 
 export const Consumer = Context.Consumer;
